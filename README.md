48ec2f95dcacc20a83909d70e3b2d43a95f53398
Dainius <dainius.skrivelis@gmail.com>
2022-02-19 21:30:34 +0200

    First draft of the UI

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # game
 Rock, paper, scissors game
 This is the assignment from theodinproject.com - I'm creating "Rock, Paper, Scissors" game.
+Images are from pexels.com, credits to: designecologist, Krolina Grabowska and pixabay.
--- js/script.js
+++ js/script.js
@@ -1,83 +1,142 @@
-// Picks random number from 1 (inclusive) to 3 (inclusive) 
-function getRandomNumber() {
-    return Math.floor(Math.random() * 3) + 1;
-}
+// // Let's play a few rounds
+// function game(timesToPlay){
+//     let userWins = 0;
+//     let computerWins = 0;
+//     for (let i = 0; i < timesToPlay; i++){
+//         let x = playRound(usersPlay(prompt("choose among: ROCK PAPER SCISSORS").toUpperCase()), computerPlay(getRandomNumber()));
+//         if (x == 1){
+//             userWins++;
+//         } else if (x == 2){
+//             computerWins++;
+//         } else if (x == 3){
+//             userWins = userWins;
+//             computerWins = computerWins;
+//         }
+//     }
+//     if(userWins > computerWins){
+//         console.log(`User wins! ${userWins} to ${computerWins}!`);
+//     } else if (userWins < computerWins) {
+//         console.log(`Computer wins ${computerWins} to ${userWins}!`)
+//     } else {
+//         console.log(`It's a tie ${computerWins} to ${userWins}!`)
+//     }
+
+//}
+
+const btn = document.querySelectorAll(".actionBtn");
+const img = document.querySelector("#humans");
+const img2 = document.querySelector("#computers");
+const roundWinner = document.querySelector("#round-winner");
+const compVic = document.querySelector("#computers-victories");
+const humVic = document.querySelector("#humans-victories");
+const ultimateWinner = document.querySelector("#winner");
+const refreshButton = document.querySelector("#refresh");
+const winnerImage = document.querySelector("#ultimateWinner");
+const main = document.querySelector("#main");
+const winnerAnnounced = document.querySelector("#winner-announced");
+
+
+refreshButton.addEventListener("click", () => location.reload());
+
 
 // Assigns selection to random number
-function computerPlay(selectedNumber){
-    if (selectedNumber == 1){
-        return "ROCK";
-    } else if (selectedNumber == 2) {
-        return "PAPER";
-    } else if (selectedNumber == 3) {
-        return "SCISSORS";
+
+function getComputersNumber() {
+    let computersNumber = Math.floor(Math.random() * 3) + 1;
+    img2.src=`./images/${computersNumber}.jpg`;
+    return computersNumber;
+
+}
+
+// Represents humans choice in a picture
+function getHumansChoice(item){
+    if (item.id == "rock"){
+        img.src="./images/1.jpg";
+        return 1;
+    } else if (item.id == "paper") {
+        img.src="./images/2.jpg";
+        return 2;
+    } else if (item.id == "scissors") {
+        img.src="./images/3.jpg";
+        return 3;
     }
 }
 
-// Gets user's input, checks if it is proper and converts it to uppercase
-function usersPlay(input){
-    if (input == "ROCK" || input == "PAPER" || input == "SCISSORS") {
-        return input;
+// Checks who won a round
+function checkWhoWonRound (playerSelection, computerSelection){
+    // outcomes 1 - user wins, 2 - computer wins, 3 - it's a tie    
+        let outcome;
+        if (playerSelection == 1 && computerSelection == 3){
+            outcome = 1;
+            roundWinner.textContent = "You win!";
+            return outcome;
+        } else if (playerSelection == 3 && computerSelection == 1){
+            outcome = 2;
+            roundWinner.textContent = "Computer wins!";
+            return outcome;
+        } else if (playerSelection == 2 && computerSelection == 3){
+            outcome = 2;
+            roundWinner.textContent = "Computer wins!";
+            return outcome;
+        } else if (playerSelection == 3 && computerSelection == 2){
+            outcome = 1;
+            roundWinner.textContent = "You win!";
+            return outcome;
+        } else if (playerSelection == 2 && computerSelection == 1){
+            outcome = 1;
+            roundWinner.textContent = "You win!";
+            return outcome;
+        } else if (playerSelection == 1 && computerSelection == 2){
+            outcome = 2;
+            roundWinner.textContent = "Computer wins!";
+            return outcome;
+        } else if (playerSelection == computerSelection) {
+            roundWinner.textContent = "It's a tie!";
+            outcome = 3;
+            return outcome;
+        }
+}
+
+
+let humanVictories = 0;
+let computerVictories = 0;
+
+function countScore(num){
+    if (num == 1){
+        humanVictories++;
+    } else if (num == 2 ){
+        computerVictories++;
     } else {
-       alert("You entered invalid selection! Try again :)");
-       return usersPlay();
+        computerVictories = computerVictories;
+        humanVictories = humanVictories;
+    }
+    humVic.textContent = `${humanVictories}`;
+    compVic.textContent = `${computerVictories}`;
+}
+
+function checkIfOver(h, com){
+    if (h == 5){
+        main.style.display = "none";
+        ultimateWinner.style.display = "flex";
+        winnerImage.src = "./images/you.jpg";
+        winnerAnnounced.textContent = "YOU WON";
+    } else if (com == 5) {
+        main.style.display = "none";
+        ultimateWinner.style.display = "flex";
+        winnerImage.src = "./images/computer.jpg";
+        winnerAnnounced.textContent = "COMPUTER WON";
     }
     
 }
 
-// Plays a round
-function playRound(playerSelection, computerSelection){
-    console.log (`Your selection: ${playerSelection}`);
-    console.log (`Computer's selection: ${computerSelection}`);
-// outcomes 1 - user wins, 2 - computer wins, 3 - it's a tie    
-    let outcome = 0;
-    if (playerSelection == "ROCK" && computerSelection == "SCISSORS"){
-        console.log("You win! ROCK beats SCISSORS!");
-        outcome = 1;
-    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
-        console.log("You lose! ROCK beats SCISSORS!")
-        outcome = 2;
-    } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
-        console.log("You lose! SCISSORS beat PAPER!");
-        outcome = 2;
-    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER"){
-        console.log("You win! SCISSORS beat PAPER!");
-        outcome = 1;
-    } else if (playerSelection == "PAPER" && computerSelection == "ROCK"){
-        console.log("You win! PAPER beats ROCK!");
-        outcome = 1;
-    } else if (playerSelection == "ROCK" && computerSelection == "PAPER"){
-        console.log("You lose! PAPER beats ROCK!");
-        outcome = 2;
-    } else if (playerSelection == computerSelection) {
-        console.log("It's a tie!");
-        outcome = 3;
-    }
-    return outcome;
-}
-// Let's play a few rounds
-function game(timesToPlay){
-    let userWins = 0;
-    let computerWins = 0;
-    for (let i = 0; i < timesToPlay; i++){
-        let x = playRound(usersPlay(prompt("choose among: ROCK PAPER SCISSORS").toUpperCase()), computerPlay(getRandomNumber()));
-        if (x == 1){
-            userWins++;
-        } else if (x == 2){
-            computerWins++;
-        } else if (x == 3){
-            userWins = userWins;
-            computerWins = computerWins;
-        }
-    }
-    if(userWins > computerWins){
-        console.log(`User wins! ${userWins} to ${computerWins}!`);
-    } else if (userWins < computerWins) {
-        console.log(`Computer wins ${computerWins} to ${userWins}!`)
-    } else {
-        console.log(`It's a tie ${computerWins} to ${userWins}!`)
-    }
 
-}
 
-game(prompt("How many times you want to play?"));
+btn.forEach(function(button) {
+    button.addEventListener("click", () =>{
+        let a = getComputersNumber();
+        let b = getHumansChoice(button);
+        let c = checkWhoWonRound(b, a);
+        countScore(c);
+        checkIfOver(humanVictories, computerVictories);
+    });
+});
